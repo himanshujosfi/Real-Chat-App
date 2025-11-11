4456fe78b5ad917fd1fd9facfc15289143beefe7
joshi <hemjoshi395@gmail.com>
2025-11-11 20:12:11 +0530

    tanstack querry added

--- frontend/src/Component/Pages/Dashboard.tsx
+++ frontend/src/Component/Pages/Dashboard.tsx
@@ -1,41 +1,67 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { ApiUrl } from "@/Common/Api";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 
 export const Dashboard = () => {
     const { userId } = useParams()
-    const getUser = async () => {
-        const res = await fetch(ApiUrl + `/getUser/${userId}`, {
-            method: "Get",
-            headers: {
-                "content-type": "application/json"
-            },
-            credentials: "include",
-        })
-        const data = await res.json()
-        console.log(data)
-    }
-    const getAllUser = async () => {
-        const res = await fetch(ApiUrl + `/allUsers`, {
-            method: "Get",
-            headers: {
-                "content-type": "application/json"
-            },
-            credentials: "include",
-        })
-        const data = await res.json()
-        console.log(data)
-    }
+    const getUser = useQuery({
+        queryKey: ["user", userId],
+        queryFn: async () => {
+            const res = await fetch(`${ApiUrl}/getUser/${userId}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+            });
+            if (!res.ok) throw new Error("Failed to fetch users");
+            return res.json();
+        },
+        enabled: !!userId,
+    });
+
+    const getAllUser = useQuery({
+        queryKey: ["getAll"],
+        queryFn: async () => {
+            const res = await fetch(ApiUrl + `/allUsers`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+            });
+            if (!res.ok) throw new Error("Failed to fetch all users");
+            return res.json();
+        },
+    });
+
 
     useEffect(() => {
-        if (userId) getUser();
-        getAllUser();
+        if (getUser.isSuccess) toast.success(" User fetched successfully");
+        if (getUser.isError) toast.error(" Failed to fetch user");
+
+        if (getAllUser.isSuccess) toast.success(" All users fetched successfully");
+        if (getAllUser.isError) toast.error(" Failed to fetch all users");
+    }, [
+        getUser.isSuccess,
+        getUser.isError,
+        getAllUser.isSuccess,
+        getAllUser.isError,
+    ]);
+
+    useEffect(() => {
+        if (userId) getUser.refetch()
+        getAllUser.refetch()
     }, [userId]);
 
     return (
         <>
-            test
+            <div>
+                {getUser.isLoading ? "Loading .." : getUser.data?.email}
+            </div>
         </>
     );
 };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify"
+
 import './index.css'
 import App from './App.tsx'
 
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer />
+      <App />
+    </QueryClientProvider>
+  </StrictMode >
 )
+
